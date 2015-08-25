839d322ae49766fcac3c60a549f8dd3f3e6abd1e
ashley baird <ashleybaird99@gmail.com>
2015-08-25 15:55:00 -0400

    seeded

--- concert/db/seeds.rb
+++ concert/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a = Venue.create({name: 'MSG', city: 'NYC', state: 'NY', family_friendly: true})
+b = Band.create({name: 'Random Stuff', genre: 'Alternative', explicity_lyrics: true})
+c = Event.create({date: 'Aug.24, 2015', alcohol_served: true, venue_id: 1, band_id: 1})	
