a55df2ad03cd86c59af58c5219422c9e43d0af93
ashley baird <ashleybaird99@gmail.com>
2015-08-25 15:46:20 -0400

    shmee

--- concert/config/routes.rb
+++ concert/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :bands
-  resources :events
-  resources :venues
+  shallow do
+   resources :venues do
+    resources :events
+    resources :bands
+   end
+  end
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
